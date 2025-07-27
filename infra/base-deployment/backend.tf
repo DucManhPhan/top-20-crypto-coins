6a514c017cb0d522a6f6b725032433c573421740
unknown <ducmanhphan93@gmail.com>
2025-07-27 22:07:48 +0700

    Fix the bucket name when init terraform

--- infra/base-deployment/backend.tf
+++ infra/base-deployment/backend.tf
@@ -1,6 +1,5 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-crypto-coins-bucket-manhpd5"
     key    = "crypto-coins/terraform.tfstate"
     region = "ap-southeast-1"
     encrypt = true
